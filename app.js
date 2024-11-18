18ce43b91e0e65ed1fac7157eb415869898b045e
Navjot420 <singhnavjot24ns@gmail.com>
2024-11-18 10:19:32 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//in the browser http://localhost:3000/user/23
+
+app.get("/user/:id", (req, res)=>{
+    const userId = req.params.id;
+    res.send('User id is${userId}');
+});
+
+app.get('/search', (req,res) =>{
+const query = req.query.q;
+res.send(`The query search term is ${query}`)
+
+})
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:${PORT}');
+});
